56a150130cb4013b79cdbd2cb88c9ec927dc703c
wesley.moody <wgm3190@gmail.com>
2020-02-25 15:21:25 -0600

    done

--- api/server.js
+++ api/server.js
@@ -1,21 +1,47 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session"); //  npm i express-session
+const KnexStore = require('connect-session-knex')(session); //   remember to curry and pass the session
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const restricted = require('../auth/restricted-middleware');
+const knex = require('../database/dbConfig'); //  needed for storing sessions in the DB
 
 const server = express();
+
+const sessionConfig = {
+  name: "monster",
+  secret: "keep it secret, keep it safe!",
+  resave: false,
+  saveUninitialized: true, // GDPR compliance
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false, //  should always be true in production
+    httpOnly: true //  true means JS can't touch the cookie, always default here
+  },
+  store: new KnexStore({
+    knex,
+    tablename: 'sessions',
+    createtable: true,
+    sidfieldname: 'sid',
+    clearInterval: 1000 * 60 * 15,  // interval when to delete expired cookies
+  }),
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig)); // Turn on session middleware
+//  at this point there is a req.session object created by express-session
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", restricted, usersRouter);
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.get("/", (req, res) => {
+  // console.log(req.session);
+  res.json({ api: "up" });
 });
 
 module.exports = server;
